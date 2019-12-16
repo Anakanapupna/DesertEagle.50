e0e0e976f75e5a69d5e03e4adcc7095c4ebadfb6
Seniscram <57326590+Seniscram@users.noreply.github.com>
2019-12-16 15:48:12 -0600

    Add files via upload

---
+++ BulletToothTony.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+ECHO Vinny: I don't want a fuss, and I don't want to put a bullet in your face, 
+ECHO but if you don't give me exactly what I want, 
+ECHO there will be ****ing murders.
+PAUSE
